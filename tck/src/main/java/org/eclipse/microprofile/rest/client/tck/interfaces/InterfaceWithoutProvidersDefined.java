f8652223823dbaed164abead10fba488a5ffdf25
John D. Ament <john.d.ament@gmail.com>
2017-10-30 19:45:31 -0400

    Adding TCK tests for the build client.

---
+++ tck/src/main/java/org/eclipse/microprofile/rest/client/tck/interfaces/InterfaceWithoutProvidersDefined.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.eclipse.microprofile.rest.client.tck.interfaces;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/")
+@Produces(MediaType.TEXT_PLAIN)
+@Consumes(MediaType.TEXT_PLAIN)
+public interface InterfaceWithoutProvidersDefined {
+    @POST
+    Response executePost(String body);
+
+    @PUT
+    @Path("/{id}")
+    Response executePut(@PathParam("id") String id, String body);
+}
