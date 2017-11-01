8fd7f159a6e241b8c5098623430ca86c737d6cdc
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2017-11-01 12:02:10 +0100

    Reverted unnecessary changes Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

--- api/src/main/java/org/eclipse/microprofile/rest/client/RestClientBuilder.java
+++ api/src/main/java/org/eclipse/microprofile/rest/client/RestClientBuilder.java
@@ -26,24 +26,17 @@ import java.util.function.ToIntFunction;
 
 /**
  * <p>
- * This is the main entry point for creating a Type Safe Rest Client.
- * Implementations are expected to implement this class and register a service
- * provider via {@link ServiceLoader} that works within their implementation.
+ * This is the main entry point for creating a Type Safe Rest Client.  Implementations are expected
+ * to implement this class and register a service provider via {@link ServiceLoader} that works within their implementation.
  * <p>
- * Invoking
- * <pre>RestClientBuilder.newBuilder()</pre> is intended to always create a new
- * instance, not use a cached version.
+ * Invoking <pre>RestClientBuilder.newBuilder()</pre> is intended to always create a new instance, not use a cached version.
  * <p>
- * If multiple implementations of RestClientBuilder are discovered, the one with
- * the highest value of the {@link Priority} annotation is used
+ * If multiple implementations of RestClientBuilder are discovered, the one with the highest value of the {@link Priority} annotation is used
  * <p>
- * The {@link ServiceLoader} will first search via the current Thread's Context
- * ClassLoader, then {@link RestClientBuilder}'s {@link ClassLoader}
+ * The {@link ServiceLoader} will first search via the current Thread's Context ClassLoader, then {@link RestClientBuilder}'s {@link ClassLoader}
  * <p>
- * The
- * <pre>RestClientBuilder</pre> is a {@link Configurable} class as defined by
- * JAX-RS. This allows a user to register providers, implementation specific
- * configuration.
+ * The <pre>RestClientBuilder</pre> is a {@link Configurable} class as defined by JAX-RS.  This allows a user to register providers,
+ * implementation specific configuration.
  */
 public interface RestClientBuilder extends Configurable<RestClientBuilder> {
 
@@ -62,31 +55,21 @@ public interface RestClientBuilder extends Configurable<RestClientBuilder> {
     }
 
     /**
-     * Specifies the base URL to be used when making requests. Assuming that the
-     * interface has a
-     * <pre>@Path("/api")</pre> at the interface level and a
-     * <pre>url</pre> is given with
-     * <pre>http://my-service:8080/service</pre> then all REST calls will be
-     * invoked with a
-     * <pre>url</pre> of
-     * <pre>http://my-service:8080/service/api</pre> in addition to any
-     * <pre>@Path</pre> annotations included on the method.
-     *
+     * Specifies the base URL to be used when making requests.  Assuming that the interface has a <pre>@Path("/api")</pre> at the interface level
+     * and a <pre>url</pre> is given with <pre>http://my-service:8080/service</pre> then all REST calls will be invoked with a <pre>url</pre> of
+     * <pre>http://my-service:8080/service/api</pre> in addition to any <pre>@Path</pre> annotations included on the method.
      * @param url the base Url for the service.
      * @return the current builder with the baseUrl set
      */
     public abstract RestClientBuilder baseUrl(URL url);
 
     /**
-     * Based on the configured RestClientBuilder, creates a new instance of the
-     * given REST interface to invoke API calls against.
-     *
+     * Based on the configured RestClientBuilder, creates a new instance of the given REST interface to invoke API calls against.
      * @param clazz the interface that defines REST API methods for use
      * @param <T> the type of the interface
      * @return a new instance of an implementation of this REST interface that
-     * @throws IllegalStateException if not all pre-requisites are satisfied for
-     * the builder, this exception may get thrown. For instance, if a URL has
-     * not been set.
+     * @throws IllegalStateException if not all pre-requisites are satisfied for the builder, this exception may get thrown.  For instance, if a URL
+     *  has not been set.
      */
     public abstract <T> T build(Class<T> clazz) throws IllegalStateException;
 
