7fbc23880b0e18893f425334307b5225f1da28aa
Ondrej Mihalyi <ondrej.mihalyi@gmail.com>
2017-11-29 16:03:51 +0100

    Fixes #21 - a separate SPI for providing RestClientBuilder instances Signed-off-by:Ondrej Mihalyi <ondrej.mihalyi@gmail.com>

--- api/src/main/java/org/eclipse/microprofile/rest/client/RestClientBuilder.java
+++ api/src/main/java/org/eclipse/microprofile/rest/client/RestClientBuilder.java
@@ -9,55 +9,37 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either excodess or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 package org.eclipse.microprofile.rest.client;
 
-import javax.annotation.Priority;
 import javax.ws.rs.core.Configurable;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.ServiceLoader;
-import java.util.function.ToIntFunction;
+import org.eclipse.microprofile.rest.client.spi.RestClientBuilderResolver;
 
 /**
+ * This is the main entry point for creating a Type Safe Rest Client.
  * <p>
- * This is the main entry point for creating a Type Safe Rest Client.  Implementations are expected
- * to implement this class and register a service provider via {@link ServiceLoader} that works within their implementation.
+ * Invoking {@link newBuilder()} is intended to always create a new instance, not use a cached version.
  * <p>
- * Invoking <pre>RestClientBuilder.newBuilder()</pre> is intended to always create a new instance, not use a cached version.
- * <p>
- * If multiple implementations of RestClientBuilder are discovered, the one with the highest value of the {@link Priority} annotation is used
- * <p>
- * The {@link ServiceLoader} will first search via the current Thread's Context ClassLoader, then {@link RestClientBuilder}'s {@link ClassLoader}
- * <p>
- * The <pre>RestClientBuilder</pre> is a {@link Configurable} class as defined by JAX-RS.  This allows a user to register providers,
+ * The <code>RestClientBuilder</code> is a {@link Configurable} class as defined by JAX-RS.  This allows a user to register providers,
  * implementation specific configuration.
+ * <p>
+ * Implementations are expected
+ * to implement this class and provide the instance via the mechanism in {@link RestClientBuilderResolver.instance()}.
  */
 public interface RestClientBuilder extends Configurable<RestClientBuilder> {
 
     public static RestClientBuilder newBuilder() {
-        ServiceLoader<RestClientBuilder> loader = ServiceLoader.load(RestClientBuilder.class);
-        List<RestClientBuilder> clientBuilders = new ArrayList<>();
-        loader.forEach(clientBuilders::add);
-        loader = ServiceLoader.load(RestClientBuilder.class, RestClientBuilder.class.getClassLoader());
-        loader.forEach(clientBuilders::add);
-
-        if (clientBuilders.size() == 0) {
-            throw new RuntimeException("No implementation of '" + RestClientBuilder.class.getSimpleName() + "' found");
-        }
-        clientBuilders.sort(Comparator.comparingInt(PrivateRestClientBuilder.priorityComparator()).reversed());
-        return clientBuilders.get(0);
+        return RestClientBuilderResolver.instance().newBuilder();
     }
 
     /**
-     * Specifies the base URL to be used when making requests.  Assuming that the interface has a <pre>@Path("/api")</pre> at the interface level
-     * and a <pre>url</pre> is given with <pre>http://my-service:8080/service</pre> then all REST calls will be invoked with a <pre>url</pre> of
-     * <pre>http://my-service:8080/service/api</pre> in addition to any <pre>@Path</pre> annotations included on the method.
+     * Specifies the base URL to be used when making requests.  Assuming that the interface has a <code>@Path("/api")</code> at the interface level
+     * and a <code>url</code> is given with <code>http://my-service:8080/service</code> then all REST calls will be invoked with a <code>url</code> of
+     * <code>http://my-service:8080/service/api</code> in addition to any <code>@Path</code> annotations included on the method.
      * @param url the base Url for the service.
      * @return the current builder with the baseUrl set
      */
@@ -68,28 +50,9 @@ public interface RestClientBuilder extends Configurable<RestClientBuilder> {
      * @param clazz the interface that defines REST API methods for use
      * @param <T> the type of the interface
      * @return a new instance of an implementation of this REST interface that
-     * @throws IllegalStateException if not all pre-requisites are satisfied for the builder, this exception may get thrown.  For instance, if a URL
+     * @throws IllegalStateException if not all code-requisites are satisfied for the builder, this exception may get thrown.  For instance, if a URL
      *  has not been set.
      */
     public abstract <T> T build(Class<T> clazz) throws IllegalStateException;
 
-}
-
-class PrivateRestClientBuilder {
-    
-    private PrivateRestClientBuilder() {
-        // shouldn't be instantiated
-    }
-
-    static ToIntFunction<Object> priorityComparator() {
-        return value -> {
-            Priority priority = value.getClass().getAnnotation(Priority.class);
-            if (priority == null) {
-                return 1;
-            }
-            else {
-                return priority.value();
-            }
-        };
-    }
 }
