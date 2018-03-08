f1c9f9bbd75ce4aeb0ddb332f133464dacb75a50
Andy McCright <j.andrew.mccright@gmail.com>
2018-03-08 13:45:15 -0600

    TCK verify @Dependent is no longer required
    Signed-off-by: Andy McCright <j.andrew.mccright@gmail.com>

--- tck/src/main/java/org/eclipse/microprofile/rest/client/tck/interfaces/SimpleGetApi.java
+++ tck/src/main/java/org/eclipse/microprofile/rest/client/tck/interfaces/SimpleGetApi.java
@@ -18,13 +18,11 @@ package org.eclipse.microprofile.rest.client.tck.interfaces;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
-import javax.enterprise.context.Dependent;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
 @Path("/")
-@Dependent
 @RegisterRestClient
 public interface SimpleGetApi {
     @GET
