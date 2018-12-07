d38ea6de9b76adc0078860198841b8c38a017cbc
Andy McCright <j.andrew.mccright@gmail.com>
2018-12-07 14:05:22 -0600

    Issues #5 and #107: Clarify integration with CDI and FT
    Includes new "Integration" section in the spec document
    and new TCK test cases for CDI interceptor support.
    
    Signed-off-by: Andy McCright <j.andrew.mccright@gmail.com>

---
+++ tck/src/main/java/org/eclipse/microprofile/rest/client/tck/interfaces/LoggableInterceptor.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.eclipse.microprofile.rest.client.tck.interfaces;
+
+import java.lang.reflect.Method;
+
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+@Loggable @Interceptor
+public class LoggableInterceptor {
+
+    private static String invocationMessage;
+
+    public static String getInvocationMessage() {
+        return invocationMessage;
+    }
+
+    public static void setInvocationMessage(String msg) {
+        invocationMessage = msg;
+    }
+
+    @AroundInvoke
+    public Object logInvocation(InvocationContext ctx) throws Exception {
+        Method m = ctx.getMethod();
+        invocationMessage = m.getDeclaringClass().getName() + "." + m.getName();
+        System.out.println("Invoking " + invocationMessage);
+        try {
+            Object returnVal = ctx.proceed();
+            invocationMessage += " " + returnVal;
+            return returnVal;
+        }
+        finally {
+            System.out.println("Invoked " + invocationMessage);
+        }
+    }
+}
