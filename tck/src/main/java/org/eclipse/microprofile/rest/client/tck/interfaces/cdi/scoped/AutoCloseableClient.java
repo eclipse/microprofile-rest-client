eb0cf2ba78bfb23edcffd42baba1ca0d78ee4ec0
James R. Perkins <jperkins@redhat.com>
2024-05-13 12:10:29 -0700

    [348] Clarify that when a client injected as a CDI bean falls out of scope, the client is closed. Add TCK tests to assert this behavior.
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

---
+++ tck/src/main/java/org/eclipse/microprofile/rest/client/tck/interfaces/cdi/scoped/AutoCloseableClient.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.eclipse.microprofile.rest.client.tck.interfaces.cdi.scoped;
+
+import jakarta.ws.rs.Path;
+import org.eclipse.microprofile.rest.client.annotation.RegisterProvider;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+@Path("/")
+// The host/port is hard-coded here as it is not currently used. The StringClientRequestFilter is used to abort the
+// request with a 200 response code.
+@RegisterRestClient(baseUri = "http://localhost:8080")
+@RegisterProvider(StringClientRequestFilter.class)
+public interface AutoCloseableClient extends StringClient, AutoCloseable {
+}
